fa461bd90d587bbb146f151112c293f2e6487169
ahmediai809183-hub <ahmed.i.ai809183@gmail.com>
2026-06-27 15:09:09 +0300

    Update email_validator.py

--- email_validator.py
+++ email_validator.py
@@ -2,18 +2,13 @@ import requests
 
 class EmailValidator:
     def __init__(self, api_key=None):
-        # هنا حط رابط الـ Railway بتاعك اللي جربناه وطلع الـ JSON
-        self.base_url = "https://public-production-242b.up.railway.app/validate"
         self.api_key = api_key
+        self.base_url = "https://email-validator.lemonsqueezy.com/api/v1/check"
 
     def check(self, email):
-        headers = {}
-        if self.api_key:
-            headers["X-API-Key"] = self.api_key
-        
         params = {"email": email}
-        try:
-            response = requests.get(self.base_url, params=params, headers=headers)
-            return response.json()
-        except Exception as e:
-            return {"status": "error", "message": str(e)}
+        if self.api_key:
+            params["api_key"] = self.api_key
+        
+        response = requests.get(self.base_url, params=params)
+        return response.json()
